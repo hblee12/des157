2091d274dfd9012f86c61909bc2dee862cc27e2d
hblee12 <hbllee@ucdavis.edu>
2017-01-16 21:37:31 -0800

    Initial commit

--- studio1/script.js
+++ studio1/script.js
@@ -4,26 +4,19 @@
 // like print() messages in Processing
 console.log("this is a js comment");
 
+
+var cnv;
+var d;
+var g;
 function setup() {
   // here we use a callback to display the image after loading
-  createCanvas(800, 250);
+  cnv = createCanvas(800, 250);
   background(255);
-  img = createImage(230, 230);
-  img.loadPixels();
-  for(var x = 0; x < img.width; x++) {
-    for(var y = 0; y < img.height; y++) {
-      var a = map(y, 0, img.height, 255, 0);
-      img.set(x, y, [255, a]); 
-    }
-  }
-  img.updatePixels();
+  cnv.mouseOver(changeImage);
+  d = 10;    
 }
 
 function draw(){
-   background(0);
-   image(img, 90, 80);
-   image(img, mouseX-img.width/2, mouseY-img.height/2);
-    
     loadImage("banner_icons_1.png", function(img) {
     image(img, 80, 1, img.width/2, img.height/2);
   });
@@ -35,4 +28,11 @@ function draw(){
     loadImage("banner_icons_3.png", function(img) {
     image(img, 480, 1, img.width/2, img.height/2);
   });
+}
+
+function changeImage() {
+  d = d + 10;
+  if (d > 100) {
+    d = 0;
+  }
 }
