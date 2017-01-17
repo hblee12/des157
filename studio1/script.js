78b3a813c82c23c1edcdb38882273c5990b81609
hblee12 <hbllee@ucdavis.edu>
2017-01-16 20:51:19 -0800

    Initial commit

--- studio1/script.js
+++ studio1/script.js
@@ -16,10 +16,10 @@ function draw(){
   });
     
     loadImage("banner_icons_2.png", function(img) {
-    image(img, 500, 1, img.width/2, img.height/2);
+    image(img, 400, 1, img.width/2, img.height/2);
   });
     
     loadImage("banner_icons_3.png", function(img) {
-    image(img, 900, 1, img.width/2, img.height/2);
+    image(img, 800, 1, img.width/2, img.height/2);
   });
 }
