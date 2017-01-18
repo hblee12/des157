877946a3efd4f48405f3286af78ede92b865ea41
hblee12 <hbllee@ucdavis.edu>
2017-01-18 11:40:02 -0800

    Initial commit

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ function setup() {
 
 function draw(){ 
     
+     if (mouseX > 50) && (mouseX < 250){
+        loadImage("studio1/banner_icons_2.png", function(img) {
+        image(img, 50, 1, img.width/2, img.height/2);
+        });
+    }
+    
     loadImage("studio1/banner_icons_1.png", function(img) {
     image(img, 50, 1, img.width/2, img.height/2);
   });
@@ -26,12 +32,6 @@ function draw(){
     loadImage("studio1/banner_icons_3.png", function(img) {
     image(img, 480, 1, img.width/2, img.height/2);
   });
-    
-    if (mouseX > 50) && (mouseX < 250){
-        loadImage("studio1/banner_icons_2.png", function(img) {
-        image(img, 50, 1, img.width/2, img.height/2);
-        });
-    }
     
 }
 
