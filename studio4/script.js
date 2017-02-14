3f4c42d23a2bf754b35026a9fc322be7a7e37ed5
hblee12 <hbllee@ucdavis.edu>
2017-02-14 11:46:00 -0800

    Initial commit

--- studio4/script.js
+++ studio4/script.js
@@ -2,30 +2,72 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
     var one = document.getElementById('one');
-    var answer = document.getElementById('answer');
-    var close = document.getElementById('close');
+    var tip = document.getElementById('tip');
+    var two = document.getElementById('two');
+    var tip2 = document.getElementById('tip2');
+    var three = document.getElementById('three');
+    var tip3 = document.getElementById('tip3');
+    var four = document.getElementById('four');
+    var tip4 = document.getElementById('tip4');
+    var audio = document.getElementById('audio');
+    
 
-
-    one.addEventListener('click', function () {
-        answer.style.display = "block";
-        // add a fade up?
-    })
-
-    close.addEventListener('click', function () {
-        answer.style.display = "none";
-    })
-
-
+    //audio
+    one.addEventListener('click', play);
+    function play(){
+        var audio = document.getElementById("audio");
+        audio.play();
+    }
+    
+    //button change
     one.addEventListener('mousedown', function () {
         one.style.backgroundColor = 'green';
     });
+
+    one.addEventListener('mouseup', function () {
+        one.style.backgroundColor = 'green';
+    });
     
-    one.addEventListener('mouseup', function (){
-        one.style.backgroundColor = 'white';
+    two.addEventListener('mousedown', function () {
+        two.style.backgroundColor = 'green';
     });
 
-    var two = document.getElementById('two');
-    var tip2 = document.getElementById('tip2');
+    two.addEventListener('mouseup', function () {
+        two.style.backgroundColor = 'green';
+    });
+    
+    three.addEventListener('mousedown', function () {
+        three.style.backgroundColor = 'green';
+    });
+
+    three.addEventListener('mouseup', function () {
+        three.style.backgroundColor = 'green';
+    });
+    
+    four.addEventListener('mousedown', function () {
+        four.style.backgroundColor = 'green';
+    });
+
+    four.addEventListener('mouseup', function () {
+        four.style.backgroundColor = 'green';
+    });
+
+    //tips
+    one.addEventListener('mouseover', function () {
+        console.log('mouseover');
+        tipTimer = setTimeout(showtip, 100);
+    });
+
+    one.addEventListener('mouseout', function () {
+        console.log('mouseout');
+        clearTimeout(tipTimer);
+        tip.style.opacity = 0;
+    });
+
+    function showtip() {
+        console.log('message');
+        tip.style.opacity = 1;
+    }
 
     two.addEventListener('mouseover', function () {
         console.log('mouseover');
@@ -43,9 +85,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
         tip2.style.opacity = 1;
     }
 
-    var three = document.getElementById('three');
-    var tip3 = document.getElementById('tip3');
-
     three.addEventListener('mouseover', function () {
         console.log('mouseover');
         tipTimer = setTimeout(showtip3, 500);
@@ -61,9 +100,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
         console.log('message');
         tip3.style.opacity = 1;
     }
-
-    var four = document.getElementById('four');
-    var tip4 = document.getElementById('tip4');
 
     four.addEventListener('mouseover', function () {
         console.log('mouseover');
